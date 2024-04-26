debdc04ea8d8e97e7e0ec8a58c47eff9be6bd683
Vinay Kumar Pandey <147065919+VinayPandey557@users.noreply.github.com>
2024-04-26 22:24:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
 const zod = require("zod");
-const app = express();
 
-const schema = zod.array(zod.number());
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+function signJwt(username, password){
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }
+
+   const signature = jwt.sign({
+    username
+   }, jwtPassword)
+   return signature;
+}
 
 
-app.use(express.json());
+const ans = signJwt("vinay@gmail.com", "asads")
+console.log(ans)
 
-app.post("/health-checkup", function(req, res){
-    const kidneys = req.body.kidneys;
-   const response = schema.safeParse(kidneys)
-
-    res.send({
-        response
-    });
-});
-
-app.listen(6000);
