bb33518e1bf97841c235e3821ab2e9f061ef6a41
Vinay Kumar Pandey <147065919+VinayPandey557@users.noreply.github.com>
2024-04-27 23:52:51 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const zod = require("zod");
+
+
+const app = express();
+
+function userMiddleware(req, res, next){
+    if(username != "harkirat" && password != "pass"){
+        res.status(403).json({
+            msg: "Incorrect input",
+        });
+    } else {
+        next();
+    }
+};
+
+function kidneyMiddleware(req, res, next){
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(403).json({
+            msg: "Incorrect input",
+        })
+
+    }
+    else {
+        next();
+    }
+};
+
+app.get("/healthy-checkup", userMiddleware, kidneyMiddleware, function(req, res){
+    res.send("Your heart is healthy");
+
+});
+
+app.get("/kidney-check", userMiddleware, kidneyMiddleware, function(req, res){
+    res.send("Your heart is healthy");
+});
+
+app.get("/heart-check", userMiddleware, function(req, res){
+    res.send("Your heart is healthy");
+});
+
+app.listen(5000);
